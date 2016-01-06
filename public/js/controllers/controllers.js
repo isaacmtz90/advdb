1c7eea529138a770a104dff593450ee9e72c75c0
carlos <carlos.sanchez@squadventure.com>
2016-01-06 22:42:44 +0800

    updating the app

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -3,6 +3,145 @@
 /* Controllers */
 var Controllers = angular.module('advdb.controllers', []);
 
-Controllers.controller('loginCtrl', ['$scope','$http',function($scope,$http) {
-	console.log('login ctrl');
+Controllers.controller('loginCtrl', ['$scope','$http','Login',function($scope,$http,Login) {
+	Login.success = function(){
+		console.log( Login );
+		window.location = '#/home';
+	};
+	Login.fail = function(){
+		alert('fail');
+	};
+}]);
+
+Controllers.controller('homeCtrl', ['$scope','$http','Data','Login',function($scope,$http,Data,Login) {
+	if( !Login.user.profile ){
+		console.log(Login.user,"????");
+		Data.sendUser(Login.user).success(function( response ){
+			if( response.user.name ){
+				Login.user.profile = response.user;
+			}else{
+				window.location = '#/profile';
+			}
+		});
+	}
+	$scope.matchNow = function(){
+		window.location = '#/matches';
+	};
+}]);
+
+Controllers.controller('profileCtrl', ['$scope','$http','Data','Login',function($scope,$http,Data,Login) {
+	$scope.profile = {
+		loading: true,
+		profile: null,
+		validate: false,
+		showError: false,
+		disabled: false
+	};
+
+	if( !Login.user.profile ){
+		$scope.profile.profile = {
+			id: Login.user.id,
+			name: Login.user.name,
+			interestIn: "",
+			physicalAppearance: {
+				gender: "",
+				height: 0,
+				weight: 0,
+				country: ""
+			},
+			likes: {
+				movies: [],
+				music: [],
+				tvSeries: []
+			}
+		};
+	}else{
+		$scope.profile.profile = JSON.parse(JSON.stringify(Login.user.profile));
+	}
+
+	$scope.save = function(){
+		$scope.profile.validate = true;
+
+		if( !$scope.profile.profile.physicalAppearance.gender ){
+			$scope.profile.showError = true;
+			return;
+		}
+		if( !$scope.profile.profile.interestedIn ){
+			$scope.profile.showError = true;
+			return;
+		}
+		if( !$scope.profile.profile.physicalAppearance.height ){
+			$scope.profile.showError = true;
+			return;
+		}
+		if( !$scope.profile.profile.physicalAppearance.weight ){
+			$scope.profile.showError = true;
+			return;
+		}
+		if( !$scope.profile.profile.physicalAppearance.country ){
+			$scope.profile.showError = true;
+			return;
+		}
+
+		$scope.profile.disabled = true;		
+		Data.saveUser( Login.user.id , $scope.profile.profile ).success(function( response ){
+			if( response.success ){
+				Login.user.profile = response.user;
+				window.location = '#/home';
+			}
+		});
+	};	
+
+	Data.getLikes().success(function( likes ){
+		$scope.profile.likes = likes;
+		$scope.$digest();
+	});
+}]);
+
+Controllers.controller('matchesCtrl', ['$scope','$http','Data','Login',function($scope,$http,Data,Login) {
+	if( !Login.user.profile ){
+		window.location = '#/home';
+	}
+
+	$scope.suggestions = {
+		suggestions: null
+	};
+
+	$scope.like = function(){
+		$scope.suggestions.disabled = true;
+		Data.like( Login.user.id , $scope.suggestions.prospect.id ).success(function(){
+			$scope.suggestions.prospect = $scope.suggestions.suggestions.pop();
+			$scope.suggestions.disabled = false;
+			$scope.$digest();
+			if( !$scope.suggestions.prospect ){
+				//get more suggestions
+			}
+		});
+	};
+
+	$scope.dislike = function(){
+		$scope.suggestions.disabled = true;
+		Data.like( Login.user.id , $scope.suggestions.prospect.id , true ).success(function(){
+			$scope.suggestions.suggestions = $scope.suggestions.suggestions.filter(function(a){
+				$scope.suggestions.disabled = false;
+				$scope.suggestions.prospect = $scope.suggestions.suggestions.pop();
+				$scope.$digest();
+				if( !$scope.suggestions.prospect ){
+					//get more suggestions
+				}
+			});
+		});
+	};
+
+	$scope.profile = function( element ){
+		window.open( 'http://www.facebook.com/' + element.item.id );
+	};
+
+
+	Data.getMatches( Login.user.id , {} ).success(function( response ){
+		$scope.suggestions.suggestions = response.suggestions;
+		$scope.suggestions.matches = response.matches;
+		$scope.suggestions.prospect = $scope.suggestions.suggestions.pop();
+		$scope.$digest();
+	});
 }]);
--- public/js/services/services.js
+++ public/js/services/services.js
@@ -8,14 +8,164 @@ window.getService = function(service){
 /* Services */
 var Services = angular.module('advdb.services', []);
 
-Services.service('myService',function(){
-	return {
-		my: "service"
+Services.service('Login',function(){
+	var result = {
+		user: undefined,
+		scopes: [],
+		registerScope: function( scope ){
+			this.scopes.push(scope);
+		}
 	};
+
+	function statusChangeCallback(response) {
+		if (response.status === 'connected') {
+			FB.api('/me', function(response) {
+				result.user = response;
+				FB.api("/" + result.user.id + "/picture",
+				    function (response) {
+				    	if (response && !response.error) {
+				    		result.user.profilePic = response.data.url;
+				    		for( var i = 0 ; i < result.scopes.length ; i++ )
+				    			result.scopes[i].$digest();
+				    	} 
+				    }
+				);
+				if( result.success )
+					result.success();
+			});
+		} else if (response.status === 'not_authorized') {
+			if( result.fail )
+				result.fail();
+		} else {
+			if( result.fail )
+				result.fail();
+		}
+	}
+
+	window.checkLoginState = function() {
+		FB.getLoginStatus(function(response) {
+			statusChangeCallback(response);
+		});
+	}
+
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId: '164561877237144',
+			xfbml: true,
+			version: 'v2.5'
+		});
+	};
+
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	return result;
 });
 
-Services.service('Login',function(){
-	return {
-		logged: true
+Services.service('Data',function(){
+	function new_Endpoint(){
+		return {
+			callback: function(){},
+			callbackError: function(){},
+			success: function( callback ){
+				this.callback = callback;
+				return this;
+			},
+			error: function( callback ){
+				this.callback = callback;
+				return this;
+			},
+			_fakeResponse: function( data ){
+				var _self = this;
+				setTimeout(function(){
+					_self.callback( data );
+				},1000);
+			}
+		};
 	};
+
+	var _fakeUSer = {
+		id: "10153733372723548",
+		name: "Carlos Sanchez",
+		interestedIn: "female",
+		physicalAppearance: {
+			gender: "male",
+			height: 172,
+			weight: 80,
+			country: "Honduras"
+		},
+		likes: {
+			movies: ["Dawn of The Dead","Star Wars"],
+			music: ["Red Hot Chilli Peppers","Disturbed"],
+			tvSeries: []
+		}
+	};
+
+
+	var Data = {
+		sendUser: function( user ){
+			console.log('UserID',user.id);
+			var endpoint = new_Endpoint();
+			endpoint._fakeResponse({
+				success: true,
+				user: _fakeUSer
+			});
+			return endpoint;
+		},
+		getLikes: function(){
+			var endpoint = new_Endpoint();
+			endpoint._fakeResponse({
+				genders: ["male","female","other"], 
+				interestedIn: ["male","female","other","all"], 
+				movies: ["Titanic","Star Wars","Dawn of the Dead"],
+				tvSeries: ["Lost","The Walking Dead","The Simpsons"],
+				music: ["The beatles","Disturbed","Slipknot"],
+				countries: ["Honduras","United States","Guatemala","Nicaragua","El Salvador"]
+			});
+			return endpoint;
+		},
+		saveUser: function( id , profile ){
+			console.log('UserID',id, profile);
+			var endpoint = new_Endpoint();
+			endpoint._fakeResponse({
+				success: true,
+				user: _fakeUSer
+			});
+			return endpoint;
+		},
+		getMatches: function( id , filters ){
+			console.log('UserID',id, filters);
+			var endpoint = new_Endpoint();
+			endpoint._fakeResponse({
+				suggestions: [{
+					id:'100000304973925'
+				},{
+					id:'100000170168144'
+				},{
+					id:'502184878'
+				},{
+					id:'100001524085465'
+				}],
+				matches: [{
+					id:'574890965'
+				}]
+			});
+			return endpoint;
+		},
+		like: function( userId , otehrId , dislike ){
+			console.log('UserID',userId, otehrId);
+			var endpoint = new_Endpoint();
+			endpoint._fakeResponse({
+				success: true,
+				match: true
+			});
+			return endpoint;
+		}
+	};
+	return Data; 
 });
