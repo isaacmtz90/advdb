a29cf78793b73caf54623eaf5f9375756b562e07
Jherez Taylor <jherez.taylor@gmail.com>
2016-01-10 16:15:36 +0800

    Added picture_url property

--- server-flask/database/data_types.py
+++ server-flask/database/data_types.py
@@ -5,7 +5,8 @@ movie_fields = {
     'movie_id': fields.String,
     'title': fields.String,
     'year': fields.Integer,
-    'genre': fields.String
+    'genre': fields.String,
+    'picture_url': fields.String
     }
 
 
@@ -14,7 +15,8 @@ tvshow_fields = {
     'title': fields.String,
     'year': fields.Integer,
     'seasons': fields.Integer,
-    'genre': fields.String
+    'genre': fields.String,
+    'picture_url': fields.String
     }
 
 likes_fields = \
@@ -22,11 +24,11 @@ likes_fields = \
      'tvshows_liked': fields.List(fields.Nested(tvshow_fields))}
 
 movie_list = {
-    'movies' : fields.List(fields.Nested(movie_fields))    
+    'movies' : fields.List(fields.Nested(movie_fields))
 }
 
 tvshow_list = {
-    'tv_shows' : fields.List(fields.Nested(tvshow_fields))    
+    'tv_shows' : fields.List(fields.Nested(tvshow_fields))
 }
 
 
@@ -40,7 +42,7 @@ user_fields = {
     'height': fields.Integer,
     'likes': fields.Nested(likes_fields)
     }
-    
+
 
 
 class Person(object):
@@ -57,31 +59,31 @@ class Person(object):
         return self.person_id+ self.name
     def to_JSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
-    
+
 class Movie(object):
-    def __init__(self, movie_id=None, title=None, year=None, genre=None):
+    def __init__(self, movie_id=None, title=None, year=None, genre=None, picture_url=None):
         self.movie_id =movie_id
         self.title=title
         self.year=year
         self.genre=genre
-      
+        self.picture_url=picture_url
+
     def  __str__(self):
         return self.movie_id+ self.title
-    
+
 class TVShow(object):
-    def __init__(self, tvshow_id=None, title=None, year=None, genre=None, seasons =None):
+    def __init__(self, tvshow_id=None, title=None, year=None, genre=None, seasons=None, picture_url = None):
         self.tvshow_id =tvshow_id
         self.title=title
         self.year=year
         self.genre=genre
         self.seasons= seasons
-      
+        self.picture_url=picture_url
+
     def  __str__(self):
         return self.tvshow_id+ self.title
-    
+
 class Likes(object):
     def __init__(self, movies_liked=[], tvshows_liked=[]):
         self.movies_liked =movies_liked
-        self.tvshows_liked=tvshows_liked      
-      
-
+        self.tvshows_liked=tvshows_liked
